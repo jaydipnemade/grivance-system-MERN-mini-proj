3985536b8c4e8295d0cccb589388670144392f5c
JAYDIP NEMADE <jaydipnemade@gmail.com>
2023-06-18 17:15:07 +0530

    react project by jaydip

--- src/component/adminLogin.js
+++ src/component/adminLogin.js
@@ -1,28 +1,82 @@
 import React from "react";
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 const AdminLogin = () => {
-    return (<>
-        <div className="my-5 temp">..</div>
-        <div className="text-light heading-text my-5">Admin Log-in</div>
-        <div className="card p-3 form-signin text-center w-25 m-auto rounded-4 ">
-            <form>
-                <img class="mb-4" src={require("../media/img/cdac-logo-sm.png")} alt="" width="72" height="57"/>
-                    <h1 class="h3 mb-3 fw-normal">Please Log-in</h1>
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                            <label for="floatingInput">Email address</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                            <label for="floatingPassword">Password</label>
-                    </div>
+  let haddSignupAction = async (e) => {
+    e.preventDefault();
 
-                  
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                   
-                    
-            </form>
-        </div>
-    </>);
+    try {
+    //   alert(email + pass);
+      const response = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, pass }),
+      });
+    //   console.log(response);
+      if (response.ok) {
+        console.log("Login successful");
+        navigate("/admindashboard");
+
+        // Perform any additional actions after successful login
+      } else {
+        console.error("Invalid credentials");
+        // Display an error message or perform any additional actions for failed login
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="my-5 temp">..</div>
+      <div className="text-light heading-text my-5">Admin Log-in</div>
+      <div className="card p-3 form-signin text-center w-25 m-auto rounded-4 ">
+        <form onSubmit={haddSignupAction}>
+          <img
+            class="mb-4"
+            src={require("../media/img/cdac-logo-sm.png")}
+            alt=""
+            width="72"
+            height="57"
+          />
+          <h1 class="h3 mb-3 fw-normal">Please Log-in</h1>
+
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <button class="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default AdminLogin;
--- src/component/signup.js
+++ src/component/signup.js
@@ -1,45 +1,178 @@
 import React from "react";
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
-    return (<>
-        <div className="my-5 temp">..</div>
-        <div className="text-light heading-text my-5">User Sign-up</div>
-        <div className="card p-3 form-signin text-center w-25 m-auto rounded-4 ">
-            <form>
-                <img className="mb-4" src={require("../media/img/cdac-logo-sm.png")} alt="" width="72" height="57"/>
-                    <h1 className="h3 mb-3 fw-normal">Please Sign-up</h1>
+  const navigate = useNavigate();
+  const [inputError, setInputError] = useState(false);
+  const inputStyle = {
+    border: inputError ? "2px solid red" : "",
+  };
 
-                    <div className="form-floating mb-3">
-                    <input type="text"  className="form-control" id="name" placeholder="Name" />
-                            <label for="name">Name</label>
-                    </div>
+  let [signup, setsignup] = useState({
+    uname: "",
+    cno: "",
+    prn: "",
+    email: "",
+    pass: "",
+    cnfpass: "",
+  });
+  // uname
+  let handleUname = (e) => {
+    let newSignup = { ...signup, uname: e.target.value };
+    setsignup(newSignup);
+  };
 
-                    <div className="form-floating mb-3">
-                    <input type="number" className="form-control" id="cno" maxlength="10" minlength="10" placeholder="Contact number" />
-                            <label for="cno">Contact Number</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                    <input  type="number" className="form-control" id="prn"  maxlength="12" minlength="12" placeholder="PRN" />
-                            <label for="prn">PRN</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="email" placeholder="name@example.com"/>
-                            <label for="email">Email address</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="password" className="form-control" id="pass" placeholder="Password"/>
-                            <label for="pass">Password</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="password" className="form-control" id="cnfpass" placeholder="Password"/>
-                            <label for="cnfpass">Confirm Password</label>
-                    </div>
+  // cno
+  let handlecno = (e) => {
+    let newcno = { ...signup, cno: e.target.value };
+    setsignup(newcno);
+  };
 
-                  
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                  
-                    
-            </form>
-        </div>
-    </>);
+  // prn
+  let handleprn = (e) => {
+    let newprn = { ...signup, prn: e.target.value };
+    // alert(e.target.value);
+    setsignup(newprn);
+  };
+
+  // email
+  let handleemail = (e) => {
+    let newemail = { ...signup, email: e.target.value };
+    setsignup(newemail);
+  };
+
+  // pass
+  let handlepass = (e) => {
+    let newpass = { ...signup, pass: e.target.value };
+    setsignup(newpass);
+  };
+
+  // cnfpass
+  let handlecnfpass = (e) => {
+    let newcnfpass = { ...signup, cnfpass: e.target.value };
+    setsignup(newcnfpass);
+  };
+  // button
+
+  let addSignupAction = (event) => {
+    event.preventDefault();
+    const encodedPassword = btoa(signup.pass);
+    const encodedcnfPassword = btoa(signup.cnfpass);
+    if (encodedPassword === encodedcnfPassword) {
+      setInputError(false);
+      let url = `http://localhost:4000/addsignup?username=${signup.uname}&ContactNo=${signup.cno}&PRN=${signup.prn}&Email=${signup.email}&pass=${encodedPassword}`;
+      fetch(url);
+      navigate("/userLogin");
+    } else {
+      setInputError(true);
+    }
+  };
+
+  return (
+    <>
+      <div className="my-5 temp">..</div>
+      <div className="text-light heading-text my-5">User Sign-up</div>
+      <div className="card p-3 form-signin text-center w-25 m-auto rounded-4 ">
+        <form onSubmit={addSignupAction}>
+          <img
+            className="mb-4"
+            src={require("../media/img/cdac-logo-sm.png")}
+            alt=""
+            width="72"
+            height="57"
+          />
+          <h1 className="h3 mb-3 fw-normal">Please Sign-up</h1>
+
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Name"
+              value={signup.uname}
+              onChange={handleUname}
+              required
+            />
+            <label for="name">Name</label>
+          </div>
+
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="cno"
+              maxlength="10"
+              minlength="10"
+              placeholder="Contact number"
+              value={signup.cno}
+              onChange={handlecno}
+              required
+            />
+            <label for="cno">Contact Number</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="prn"
+              maxlength="12"
+              minlength="12"
+              placeholder="PRN"
+              value={signup.prn}
+              onChange={handleprn}
+              required
+            />
+            <label for="prn">PRN</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="name@example.com"
+              value={signup.email}
+              onChange={handleemail}
+              required
+            />
+            <label for="email">Email address</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="pass"
+              placeholder="Password"
+              value={signup.pass}
+              onChange={handlepass}
+              style={inputStyle}
+              required
+            />
+            <label for="pass">Password</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="cnfpass"
+              placeholder="cnfirm Password"
+              value={signup.cnfpass}
+              onChange={handlecnfpass}
+              style={inputStyle}
+              required
+            />
+            <label for="cnfpass">Confirm Password</label>
+          </div>
+          {inputError && (
+            <p style={{ color: "red" }}>
+              Confirm Password and Password should same !!!
+            </p>
+          )}
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default Signup;
